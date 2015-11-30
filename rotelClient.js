c8b4f4c687578e9a6444ea8b6ba33afc50ee0126
root <root@linux-gyt6.site>
2015-11-30 22:11:06 +0100

    Got bass and treble to work

--- rotelClient.js
+++ rotelClient.js
@@ -31,11 +31,16 @@ var RotelClient = function() {
 		this.detachEventHandlers();		
 		$("#power-flipswitch").val(this.power).flipswitch('refresh');
 		$("#mute-flipswitch").val(this.mute).flipswitch('refresh');
+		$("#tone-flipswitch").val(this.tone	).flipswitch('refresh');
 		$("#volume-slider").val(this.volume).slider('refresh');
 		$("#source").val(this.inputSource).selectmenu('refresh');
 		$("#bass-slider").val(Number(this.bass)).slider('refresh');
 		$("#treble-slider").val(Number(this.treble)).slider('refresh');
+		if (this.balance != null) {
+			$("#balance-slider").val(Number(this.balance.replace('L', '-').replace('R', ''))).slider('refresh');
+		}
 		$("#lcd-display").text(this.display1 + "\n" + this.display2);
+
 
 		this.attachEventHandlers();
 	}
@@ -46,6 +51,9 @@ var RotelClient = function() {
 		self.webSocket.send(self.getCurrentSourceEvent());
 		self.webSocket.send(self.getToneEvent());
 		self.webSocket.send(self.getVolumeEvent());
+		self.webSocket.send(self.getBassEvent());
+		self.webSocket.send(self.getTrebleEvent());
+		self.webSocket.send(self.getBalanceEvent());
 		self.webSocket.send(self.getDisplayEvent());
 	}
 
@@ -57,6 +65,7 @@ var RotelClient = function() {
 	    	$("#volume-slider").unbind("change");
 	    	$("#bass-slider").unbind("change");
 	    	$("#treble-slider").unbind("change");
+	    	$("#balance-slider").unbind("change");
 	}
 
 	this.attachEventHandlers = function() {
@@ -91,23 +100,44 @@ var RotelClient = function() {
 
 	    	$("#bass-slider").on("change", function() {
 			var v = $("#bass-slider").val();
-			var a;
+			var a = null;
 			if (v < 0) {
-				a = self.bassSetEvent(v + '');
+				a = self.bassSetEvent('-' + ('0' + Math.abs(v)).slice(-2));
 			} else if (v == 0) {
 				a = self.bassSetEvent('000');
 			} else if (v > 0) {
-				a = self.bassSetEvent('+' + v);
+				a = self.bassSetEvent('+' + ('0' + Math.abs(v)).slice(-2));
 			}
-			a = self.bassUpEvent();
 			self.webSocket.send(a);
 		} );
-	    	$("#treble-slider").on("change", function() {
-			var a = self.volumeSetEvent($("#treble-slider").val());
-			self.webSocket.send(a);
-		} );
-	}
 
+	    	$("#treble-slider").on("change", function() {
+			var v = $("#treble-slider").val();
+			var a = null;
+			if (v < 0) {
+				a = self.trebleSetEvent('-' + ('0' + Math.abs(v)).slice(-2));
+			} else if (v == 0) {
+				a = self.trebleSetEvent('000');
+			} else if (v > 0) {
+				a = self.trebleSetEvent('+' + ('0' + Math.abs(v)).slice(-2));
+			}
+			self.webSocket.send(a);
+		} );
+
+	    	$("#balance-slider").on("change", function() {
+			var v = $("#balance-slider").val();
+			var a = null;
+			if (v < 0) {
+				a = self.balanceSetEvent('L' + ('0' + Math.abs(v)).slice(-2));
+			} else if (v == 0) {
+				a = self.balanceSetEvent('000');
+			} else if (v > 0) {
+				a = self.balanceSetEvent('R' + ('0' + Math.abs(v)).slice(-2));
+			}
+			self.webSocket.send(a);
+		} );
+
+	}
 
 	this.webSocket = new ReconnectingWebSocket('ws://localhost:8989/ws');
 	this.webSocket.timeoutInterval = 1000;
@@ -148,11 +178,19 @@ var RotelClient = function() {
 	this.powerOffEvent = function() { return this.createActionEvent('power_off'); }
 	this.powerSetEvent = function(v) { return this.createActionEvent('power_' + v); }
 	this.volumeSetEvent = function(v) { return this.createActionEvent('volume_' + v); }
+
 	this.bassSetEvent = function(v) { return this.createActionEvent('bass_' + v); }
 	this.bassUpEvent = function() { return this.createActionEvent('bass_up'); }
 	this.bassDownEvent = function() { return this.createActionEvent('bass_down'); }
+
 	this.trebleSetEvent = function(v) { return this.createActionEvent('treble_' + v); }
+	this.trebleUpEvent = function() { return this.createActionEvent('treble_up'); }
+	this.trebleDownEvent = function() { return this.createActionEvent('treble_down'); }
+
 	this.balanceSetEvent = function(v) { return this.createActionEvent('balance_' + v); }
+	this.balanceLeftEvent = function() { return this.createActionEvent('balance_left'); }
+	this.balanceRightEvent = function() { return this.createActionEvent('balance_right'); }
+
 	this.volumeDownEvent = function() { return this.createActionEvent('volume_down'); }
 	this.getCurrentPowerEvent = function() { return this.createActionEvent('get_current_power'); }
 	this.getCurrentSourceEvent = function() { return this.createActionEvent('get_current_source'); }
@@ -173,85 +211,98 @@ var RotelClient = function() {
 
 	var parseEvent = function(evt) {
 		console.log("server: " + evt.data);
-		var response = JSON.parse(evt.data);
-		console.log("response:" + response);
-		if (response && response.D) {
+		if (typeof evt.data == "string") {
+			var data = null;
+			try {
+				data = JSON.parse(evt.data);
+			} catch (err) {
+				console.log("Could not parse as JSON:" + JSON.stringify(evt));
+			}
+			console.log("data:" + data);
+			if (data && data.D) {
+				//most commands end with !
+				var responses = data.D.split("!");
+				if (responses) {
+					for (var i = 0; i < responses.length; i++)  {
+						console.log("responses[" + i + "]: "  + responses[i]);
 
-			//most commands end with !
-			var responses = response.D.split("!");
-			if (responses) {
-				for (var i = 0; i < responses.length; i++)  {
-					console.log("response " + i + ": "  + responses[i]);
-
-					if (self.remainingDisplayCharCount) {
-						console.log("remainingDisplayCharCount");
-						self.display2 = responses[i];
-						self.remainingDisplayCharCount = null;
-						self.stateChanged();
-						continue;
-					} else {
-						console.log("not remainingDisplayCharCount");
-					}
-					typeAndValue = responses[i].split("=");
-					console.log("typeAndValue: " + typeAndValue);
-					if (typeAndValue) {
-						var type = typeAndValue[0];
-						var value = typeAndValue[1];
-						//since Rotel's response might be split in several events
-						//by serial port JSON
-						// we must work around it below
-						if (incompleteEvent) { 
-							type = incompleteEvent + type;
-							incompleteEvent = null;
-						}
-						if (value == null) {
-							incompleteEvent = type;
-						}
-						if (value) {
-							incompleteEvent = null;
-							switch (type) {
-								case "display": 
-									var displayCharCount = Number(value.substring(0,3));
-									self.display1 = value.substring(4);
-									self.remainingDisplayCharCount = 
-										displayCharCount - self.display1.length;
-									self.remainingDisplayCharCount = 
-										(self.remainingDisplayCharCount > 0) ? 
-											self.remainingDisplayCharCount : null;
-									break;
-								case "volume":
-									self.volume = value;
-									break;
-								case "power":
-									self.power = value;
-									break;
-								case "mute":
-									self.mute = value;
-									break;
-								case "source":
-									self.inputSource = value;
-									break;
-								case "tone":
-									self.tone = value;
-									break;
-								case "bass":
-									self.bass = value;
-									break;
-								case "treble":
-									self.treble = value;
-									break;
-								case "balance":
-									self.balance = value;
-									break;
-								case "freq":
-									self.freq = value;
-									break;
-								case "play_status":
-									self.play_status = value;
-									break;
+						if (self.remainingDisplayCharCount) {
+							console.log("remainingDisplayCharCount: " + self.remainingDisplayCharCount);
+							self.display2 = responses[i].substring(0, self.remainingDisplayCharCount);
+							self.remainingDisplayCharCount = null;
+							self.stateChanged(self.remainingDisplayCharCount);
+							//in case there is new information on remainder of line
+							response = responses[i].substring(self.remainingDisplayCharCount)
+							if (response == null || response.length < 1) {
+								continue;
 							}
-							if (self.remainingDisplayCharCount == null) {
-								self.stateChanged();
+						} else {
+							console.log("not remainingDisplayCharCount");
+							response = responses[i];
+						}
+
+						console.log("response: " + response);
+						typeAndValue = response.split("=");
+						console.log("typeAndValue: " + typeAndValue);
+						if (typeAndValue) {
+							var type = typeAndValue[0];
+							var value = typeAndValue[1];
+							//since Rotel's response might be split in several events
+							// by serial port JSON
+							// we must work around it below
+							if (incompleteEvent) { 
+								type = incompleteEvent + type;
+								incompleteEvent = null;
+							}
+							if (value == null) {
+								incompleteEvent = type;
+							}
+							if (value) {
+								incompleteEvent = null;
+								switch (type) {
+									case "display": 
+										var displayCharCount = Number(value.substring(0,3));
+										self.display1 = value.substring(4);
+										self.remainingDisplayCharCount = 
+											displayCharCount - self.display1.length;
+										self.remainingDisplayCharCount = 
+											(self.remainingDisplayCharCount > 0) ? 
+												self.remainingDisplayCharCount : null;
+										break;
+									case "volume":
+										self.volume = value;
+										break;
+									case "power":
+										self.power = value;
+										break;
+									case "mute":
+										self.mute = value;
+										break;
+									case "source":
+										self.inputSource = value;
+										break;
+									case "tone":
+										self.tone = value;
+										break;
+									case "bass":
+										self.bass = value;
+										break;
+									case "treble":
+										self.treble = value;
+										break;
+									case "balance":
+										self.balance = value;
+										break;
+									case "freq":
+										self.freq = value;
+										break;
+									case "play_status":
+										self.play_status = value;
+										break;
+								}
+								if (self.remainingDisplayCharCount == null) {
+									self.stateChanged();
+								}
 							}
 						}
 					}
